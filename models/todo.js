64949d0d194ae3316c030de62c1bf9e4d3f0a30a
Collins Igeh <igehac@gmail.com>
2022-04-26 20:02:29 +0100

    Created the Todo model schema

---
+++ models/todo.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const mySchema = mongoose.Schema;
+
+const Todo = mongoose.model('Todo', mySchema({
+    actionBy: {type: String, required: true},
+    task: {type: String, required: true},
+    status: {type: String, default: 'Pending'}
+}));
+
+model.exports = Todo;
